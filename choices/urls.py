3eb4e0ea54d2e7021b851a6b0f0d4fbab2de2399
leverh <leverh@gmail.com>
2023-06-09 17:01:42 +0200

    Added choice-detail.html template for individual numbered recipe/choice

--- choices/urls.py
+++ choices/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='choices-home'),
+    path('home/', views.ChoiceListView.as_view(), name='choices-home'),
+    path('choice/<int:pk>/', views.ChoiceDetailView.as_view(), name='choices-detail'),
     path('', views.home, name='choices-home'),
     path('about/', views.about, name='choices-about'),
 ]
--- choices/views.py
+++ choices/views.py
@@ -1,32 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from . import models
-
-choices = [
-    {
-    'author': 'John Doe',
-    'title': 'Lentil Soup',
-    'preparation' : 'mix all ingredients together and cook for 30 minutes',
-    'date_posted': 'June 9, 2023'
-    },
-{
-    'author': 'Mary Jane',
-    'title': 'Vegan Burger',
-    'preparation' : 'mix all ingredients together and cook for 30 minutes',
-    'date_posted': 'June 11, 2023'
-    },
-    {
-    'author': 'Angela Lansbury',
-    'title': 'Hashbrown Casserole',
-    'preparation' : 'mix all ingredients together and cook for 30 minutes',
-    'date_posted': 'May 1, 2023'
-    },
-    {
-    'author': 'Joan Collins',
-    'title': 'Cucumber Salad',
-    'preparation' : 'mix all ingredients together and cook for 30 minutes',
-    'date_posted': 'January 9, 2023'
-    }
-]
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
 def home(request):
@@ -35,6 +9,15 @@ def home(request):
         'choices': choices
     }
     return render(request, 'choices/home.html', context)
+
+class ChoiceListView(ListView):
+    model = models.Choice
+    template_name = 'choices/home.html'
+    context_object_name = 'choices'
+    ordering = ['-date_posted']
+
+class ChoiceDetailView(DetailView):
+    model = models.Choice
 
 def about(request):
     return render(request, 'choices/about.html', {'title': 'About'})
