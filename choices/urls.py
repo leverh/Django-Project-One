32e126a1e688167acd6dfac7bf9594c3d9c7a19e
leverh <leverh@gmail.com>
2023-06-09 11:05:30 +0200

    Fixed bug that stopped the home page from loading if /home wasn't typed into address bar

--- choices/urls.py
+++ choices/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='choices-home'),
     path('', views.home, name='choices-home'),
     path('about/', views.about, name='choices-about'),
 ]
